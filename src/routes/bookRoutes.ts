d97b65819c3bdfd6293dce0056426eadc5bfe260
Mashi <abeywickramaamashi@gmail.com>
2024-08-23 22:06:40 +0530

    Feat : auth middleware

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,7 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import * as bookController from '../controller/bookController';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.post('/', (req: Request, res: Response, next: NextFunction) => bookController.createBook(req, res, next));
 router.get('/', (req: Request, res: Response, next: NextFunction) => bookController.getAllBooks(req, res, next));
